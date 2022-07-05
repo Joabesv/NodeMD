e4e2777c00cc09df9de912982a117d547e26acc8
Joabesv <joabevarjao123@gmail.com>
2022-07-05 19:48:37 -0300

    feat: add error treatment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import { readFile } from 'node:fs/promises';
 import chalk from 'chalk';
 
+const errTreatment = err => {
+  throw new Error(chalk.red(err.code, err.message));
+};
+
 const getFile = async filepath => {
   const ENCODING = 'utf-8';
   try {
-    return await readFile(filepath, ENCODING);
+    const data = await readFile(filepath, ENCODING);
+    return data;
   } catch (err) {
-    console.log('something went wrong', chalk.red(err.message));
+    console.log('something went wrong', errTreatment(err));
   }
 };
 
