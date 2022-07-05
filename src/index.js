8214e72508adc5fa6bffb4a197bf2ae7aeaa3b2e
Joabesv <joabevarjao123@gmail.com>
2022-07-05 19:22:30 -0300

    style(refactor): refactor

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-import chalk from 'chalk';
 import { readFile } from 'node:fs/promises';
+import chalk from 'chalk';
 
 const getFile = async filepath => {
   const ENCODING = 'utf-8';
-
   try {
-    const data = await readFile(filepath, ENCODING);
-    return data;
+    return await readFile(filepath, ENCODING);
   } catch (err) {
-    console.log('something went wrong', err.message);
+    console.log('something went wrong', chalk.red(err.message));
   }
 };
 
-console.log(chalk.blue('Blue this'));
-console.log('file data', '\n', chalk.green(await getFile('sample.md')));
+console.log(chalk.blue('Blue this', await getFile('sample.md')));
